0fc92afcf0330a0719273990e812631dbccc4ad7
JongBaaam <JongBaaam@gmail.com>
2024-09-18 22:41:29 +0900

    Design: tailwind extend 설정 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,27 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "text-color-dark": "var(--text-color-dark)",
+        "text-color-gray": "var(--text-color-gray)",
+        "text-color-gray-light": "var(--text-color-gray-light)",
+        "border-color-gray": "var(--border-color-gray)",
+        "color-navy": "var(--color-navy)",
+        "color-blue": "var(--color-blue)",
+        "color-blue-hover": "var(--color-blue-hover)",
+        "color-blue-light": "var(--color-blue-light)",
+        "color-green": "var(--color-green)",
+        "color-red": "var(--color-red)",
+        "color-orange": "var(--color-orange)",
+        "color-white": "var(--color-white)",
+        "color-black-5": "var(--color-black-5)",
+        "color-black-10": "var(--color-black-10)",
+      },
+      height: {
+        "container-height": "calc(100% - 100px)",
+      },
+    },
   },
   plugins: [],
 };
