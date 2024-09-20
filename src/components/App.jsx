8562bd3ce33ceabd492c081fe378c3bbe1264c11
JongBaaam <JongBaaam@gmail.com>
2024-09-20 10:15:47 +0900

    Refactor: Container 컴포넌트 삭제

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,6 @@ import { useUserStore } from "../store/store";
 import Home from "./Home/Home";
 import Dashboard from "./Dashboard/Dashboard";
 import Header from "./Header/Header";
-import Container from "./common/Container";
 import ProtectedRoute from "./ProtectedRoute";
 import NotFoundPage from "./common/NotFoundPage";
 
@@ -15,20 +14,18 @@ export default function App() {
   return (
     <>
       <Header />
-      <Container>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              !isLoggedIn ? <Home /> : <Navigate to="/dashboard" replace />
-            }
-          />
-          <Route path="/dashboard" element={<ProtectedRoute />}>
-            <Route index element={<Dashboard />} />
-          </Route>
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </Container>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            !isLoggedIn ? <Home /> : <Navigate to="/dashboard" replace />
+          }
+        />
+        <Route path="/dashboard" element={<ProtectedRoute />}>
+          <Route index element={<Dashboard />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </>
   );
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -15,25 +15,27 @@ export default function Home() {
   }
 
   return (
-    <main className="w-full max-w-screen-2xl h-full flex justify-around items-center flex-col md:flex-row">
-      <aside className="flex justify-center items-center">
-        <HomeImage />
-      </aside>
-      <section className="flex flex-col items-center md:order-first">
-        <h1 className="mb-6 text-3xl font-bold">
-          사용자에게 <span className="text-color-blue">더 나은 경험</span>을
-          제공하세요.
-        </h1>
-        <p className="mb-6 text-center font-semibold">
-          A/B 테스트로 전환율을 분석하고, <br />
-          데이터 기반으로 더 나은 UX를 제공할 수 있습니다.
-        </p>
-        <Button
-          className="bg-color-blue px-8 py-4 rounded-lg text-white font-semibold w-fit transition hover:bg-color-blue-hover"
-          onClick={handleStartButtonClick}
-          text="시작하기"
-        />
-      </section>
-    </main>
+    <div className="px-6 py-4 w-full h-container-height flex justify-center">
+      <main className="w-full max-w-screen-2xl h-full flex justify-around items-center flex-col md:flex-row">
+        <aside className="flex justify-center items-center">
+          <HomeImage />
+        </aside>
+        <section className="flex flex-col items-center md:order-first">
+          <h1 className="mb-6 text-3xl font-bold">
+            사용자에게 <span className="text-color-blue">더 나은 경험</span>을
+            제공하세요.
+          </h1>
+          <p className="mb-6 text-center font-semibold">
+            A/B 테스트로 전환율을 분석하고, <br />
+            데이터 기반으로 더 나은 UX를 제공할 수 있습니다.
+          </p>
+          <Button
+            className="bg-color-blue px-8 py-4 rounded-lg text-white font-semibold w-fit transition hover:bg-color-blue-hover"
+            onClick={handleStartButtonClick}
+            text="시작하기"
+          />
+        </section>
+      </main>
+    </div>
   );
 }
